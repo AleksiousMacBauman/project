ffab00f1d05bcd813f323c661edcd28e3490b322
Alexios <arhipov.alexey@mail.ru>
2022-12-23 11:10:04 +0300

    ffirst lesson commit, movie database with non-cycles prompt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели','');
 
-alert('Hello');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false,
+};
 
-let result = confirm('are you here?');
-console.log(result);
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
